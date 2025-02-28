8fe2c24a7a0f913ae5b937c1bd90f099bd12949c
AlaaQrajah <alaaqaraja2004@gmail.com>
2025-02-28 18:33:05 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+let sidebar_toggle = document.querySelector("#sidebar-toggle");
+let sidebar = document.getElementById("sidebar");
+let content = document.querySelector("#content");
+sidebar_toggle.addEventListener("click", () => {
+  let sidebarWidth = window.getComputedStyle(sidebar).width;
+  if (sidebarWidth === "250px") {
+    sidebar.style.width = "0";
+    content.style.marginRight = "0";
+  } else {
+    sidebar.style.width = "250px";
+    content.style.marginRight = "250px";
+  }
+});
+
+new DataTable("#example", {
+  responsive: true,
+  buttons: [
+    // {
+    // extend:'copy',
+    // text:'نسخ'
+    // },
+    // {
+    // extend:'excel',
+    // text:'اكسل'
+    // },
+    // {
+    // extend:'pdf',
+    // text:'صيغةpdf'
+    // },
+    // {
+    // extend:'print',
+    // text:'طباعة'
+    // },
+    "copy",
+    "excel",
+    "pdf",
+    "print",
+  ],
+  layout: {
+    topStart: "buttons",
+  },
+});
